12ea9167598b40c27cd1bfe7df432206224bb6e0
Pedro Alvarado Rincon <pwalvarado@gmail.com>
2014-12-17 10:38:33 -0500

    Se agrega descripcion de los metodos a implementar

--- medellinrb--tdd-kata/test/kata_test.rb
+++ medellinrb--tdd-kata/test/kata_test.rb
@@ -7,6 +7,8 @@ class KataTest < MiniTest::Test
     # this.calculadora = new Kata();
   end
 
+  # Método sumar
+  #
   def test_suma_dos_numeros
     resultado = @calculadora.sumar(10, 5)
     assert_equal resultado, 15
@@ -17,25 +19,28 @@ class KataTest < MiniTest::Test
     assert_equal resultado, 22
    end
 
+   # Metodo contar_nucleotidos
+   # http://rosalind.info/problems/dna/
    def test_Counting_DNA_Nucleotides
     resultado = @calculadora.contar_nucleotidos("AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC")
     assert_equal resultado, "20 12 17 21"
    end
-
-   def  test_transcribing_dna_into_rna
-    resultado= @calculadora.dna_into_rna("GATGGAACTTGACTACGTAAATT")
-    assert_equal resultado, "GAUGGAACUUGACUACGUAAAUU"
-   end
-
 
    def test_Counting_DNA_Nucleotides_sad_path
     resultado = @calculadora.contar_nucleotidos("GCTTTTCTTCTGCTGCCGGGCTTGTCTCTGTGTGGTTGGTGTCTGTGCGC")
     assert_equal resultado, "0 12 17 21"
    end
 
+   # Metodo conversion de cadena dna_into_rna
+   #http://rosalind.info/problems/rna/
+   def  test_transcribing_dna_into_rna
+    resultado= @calculadora.dna_into_rna("GATGGAACTTGACTACGTAAATT")
+    assert_equal resultado, "GAUGGAACUUGACUACGUAAAUU"
+   end
+
    def  test_transcribing_dna_into_rna_sad_path
     resultado= @calculadora.dna_into_rna("12345678")
     assert_equal resultado, nil
    end
-
 end
+
